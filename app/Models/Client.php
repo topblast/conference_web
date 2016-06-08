5ec5712337ee13eff4eeebd70a137705c12d7720
draytxn <draytxn1@gmail.com>
2016-06-08 09:55:03 -0400

    set primary key in client.php

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model{
+
+    protected $primaryKey="client_id";
+
     public function conferences()
     {
         return $this->hasMany('App\Models\Conference');
