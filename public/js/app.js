124fcb899d4c627545a6617327d24cb0c37920d7
BryanRam <bryan_ram@hotmail.com>
2016-06-28 11:06:16 -0400

    added frontend pagination for main home page

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['starter.controllers', 'starter.services', 'ui.router', 'ngStorage'])
+angular.module('starter', ['starter.controllers', 'starter.services', 'ui.router', 'ngStorage', 'angularUtils.directives.dirPagination'])
 
 .run(function ($rootScope, $http, $location, $localStorage) {
         // keep user logged in after page refresh
